3e8c78b914d307a76bb9f6fa5340bc7aa420f57a
André Franco <andrelgfranco@gmail.com>
2021-01-22 14:23:40 +0000

    Initial commit

---
+++ src/test/kotlin/com/estudos/kotlin/EstudosKotlinTest.kt
@@ -0,0 +1,19 @@
+package com.estudos.kotlin
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class EstudosKotlinTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
